e929d748a23f23c1ea84b494bec65b84fa2ac1b5
Felipe Cerqueira <felipecerqueirasantos123@gmail.com>
2023-06-16 05:59:54 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}> Login </Text>
+
+     
+      <textInput
+      style={styles.input}
+      OnChangeText={Setusername}
+      value={username}
+      Placeholder= "Usuario"
+      />
+  
+      
+     <touchableopacity style={styles.button} onpress={handlesubmit(HandleSignIn)}>
+     <text style={styles.buttonText}> Acessar</text>
+
+     </touchableopacity>
     </View>
   );
 }
@@ -17,4 +33,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    fontSize: 34,
+    marginBottom: 34,
+    color: '#121212',
+    fontWeight: 'bold'
+
+  },
+  input:{
+  width: '100%',
+  height: 40,
+  backgroundColor: '#FFFFFF',
+
+  },
+
 });
